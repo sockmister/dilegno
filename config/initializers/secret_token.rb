cf5a72a716ba08d762db4c130d825451888d893e
pohchiat <pohchiat@gmail.com>
2013-05-28 20:04:21 +0800

    First Commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dilegno::Application.config.secret_token = '5566463c888507066ae2f2a8780eeb55f170b73bfdc28ee5b9897cf52fe06d89afc1f902e99b0a1b27296f9b80343abc9f7b59e587eecd3e8a3c2c3f34383d3c'
