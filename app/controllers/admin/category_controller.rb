bcc102103e7d939d0778e232508dc98108e00bf8
pohchiat@gmail.com <pohchiat@gmail.com>
2013-06-06 23:44:25 +0800

    Much of category has been implemented. Mainly missing cascade deletes.

--- app/controllers/admin/category_controller.rb
+++ app/controllers/admin/category_controller.rb
@@ -2,12 +2,12 @@ class Admin::CategoryController < AdminController
 	# display a list of all categories
 	def index
 		@categories = Category.all
-		
 	end
 
 	# return a form for creating a new category
-	# def new
-	# end
+	def new
+		@category = Category.new
+	end
 
 	# create a new category
 	def create
@@ -21,15 +21,25 @@ class Admin::CategoryController < AdminController
 	# end
 
 	# return a form for updating a category
-	# def edit
-	# end
+	def edit
+		@category = Category.find(params[:id])
+	end
 
 	# update a category
 	def update
+		@category = Category.find(params[:id])
+		@category.name = params[:category][:name]
+		@category.save
+		redirect_to :action => "index"
 	end
 
 	# destroy a category
+	# cascade delete to sub_categories and products.
+	# sub_categories should be destroyed
+	# products with this category should be reassigned a new category
 	def destroy
-	end
-
+		@category = Category.find(params[:id])
+  	@category.destroy
+  	redirect_to :action => "index"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Dilegno::Application.routes.draw do
 	namespace :admin do
   	resources :category
+  	resources :featured_image
+  	resources :sub_category
+  	resources :product
 	end
 
   get "products/categories"
