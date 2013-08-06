6a58ca35492eb43a9eee24e92f6622fcb0b825b0
pohchiat@gmail.com <pohchiat@gmail.com>
2013-08-06 22:39:25 +0800

    Removed categories in footer. Decrease footer bottom padding. Query the correct sub category. Change color to text field. Handle dimensions where either length, width or height is not available.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -5,7 +5,8 @@ class ProductController < ApplicationController
 
 		@category = Category.where(:id => @product.category_id).first
 		# @category = category.name
-		@sub_category = SubCategory.where(:category_id => @category.id).first
+		# @sub_category = SubCategory.where(:category_id => @category.id).first
+		@sub_category = SubCategory.find(@product.sub_category_id)
 
 	end
 end
