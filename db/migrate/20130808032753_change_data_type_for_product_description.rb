a3c612c3d263e612282ee29f01c01f64871ac1df
pohchiat@gmail.com <pohchiat@gmail.com>
2013-08-08 12:08:04 +0800

    Changed product description to use text instead of string. Changed featured images to scale properly, and centralize.

---
+++ db/migrate/20130808032753_change_data_type_for_product_description.rb
@@ -0,0 +1,13 @@
+class ChangeDataTypeForProductDescription < ActiveRecord::Migration
+  def up
+  	change_table :products do |t|
+  		t.change :description, :text
+  	end
+  end
+
+  def down
+  	change_table :products do |t|
+  		t.change :description, :string
+  	end
+  end
+end
