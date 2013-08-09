7151d5c0ea74857eedfe4f25ac20c7b97523eef1
pohchiat@gmail.com <pohchiat@gmail.com>
2013-08-09 15:58:44 +0800

    Added simple spambot protection, using honeypot method. Added a thank you page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
 	def contact_us
-		UserMailer.contact_us(params[:email], params[:subject], params[:message]).deliver
-
-		unless User.where(:email => params[:email]).first
-			User.create(:email => params[:email], :is_subscribed => false)
+		if params[:comments] == ""
+			logger.info 'not spam'
+			UserMailer.contact_us(params[:email], params[:subject], params[:message]).deliver
+			unless User.where(:email => params[:email]).first
+				User.create(:email => params[:email], :is_subscribed => false)
+			end
 		end
+
+		render 'thanks'
 	end
 end
