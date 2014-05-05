ca7ac651c824cdd257abce26c46be1e19ae83cb1
pohchiat@gmail.com <pohchiat@gmail.com>
2014-05-05 21:58:20 +0800

    remove useless image code in featured_image model

--- app/models/featured_image.rb
+++ app/models/featured_image.rb
@@ -2,8 +2,4 @@ class FeaturedImage < ActiveRecord::Base
   attr_accessible :description, :link, :image
 
   has_one :dilegno_image, as: :imageable
-
-  has_attached_file :image, 
-  :styles => { :medium => "300x300>", :thumb => "100x100>" }, 
-  :default_url => "/images/:style/missing.png"
 end
